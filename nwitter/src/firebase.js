ef6e971eb3eb82623576e52a702772beaefce75e
totquf <dalda117@naver.com>
2022-12-31 14:37:38 +0900

    2022-12-31-2:37

---
+++ nwitter/src/firebase.js
@@ -0,0 +1,15 @@
+//수정한 코드
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTO_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
+  };
+
+export default firebase.initializeApp(firebaseConfig);
